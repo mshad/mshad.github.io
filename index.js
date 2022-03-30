1ab0f439d4be212b2c7a596f3fd73edf4a4a3f43
Matthias Markowski <mm@demodern.de>
2022-03-30 20:48:22 +0200

    Replace background image with realtime background Improve js code

--- index.js
+++ index.js
@@ -42,42 +42,65 @@ window.addEventListener("scroll", () => {
     }
 })
 
-const bg = document.getElementById('background')
+function vertexShader() {
+    return `
+    varying vec3 vUv; 
 
-bg.style['opacity'] = "0" // in case css is not yet loaded
-window.onload = () => {
-    window.addEventListener("scroll", () => {
-        bg.style['opacity'] = window.scrollY > 0 ? "0" : "1"
-    })
+    void main() {
+        vUv = position + 0.5;
+        vUv.y = 1.0 - uv.y; 
+        vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
+        gl_Position = projectionMatrix * modelViewPosition;
+    }
+  `
+}
 
-    bg.style['transition'] = "opacity 1s"
-    bg.style['opacity'] = "1"
+function fragmentShader() {
+    return `
+    varying vec3 vUv;
+    uniform float uTime;
 
-    init()
-    initParticles()
-    animate()
-
-    anime.timeline({loop: false})
-        .add({
-            targets: '.word',
-            scale: [2, 1],
-            opacity: [0, 1],
-            translateY: ["50px", 0],
-            easing: 'easeOutCubic',
-            duration: 600,
-            delay: (el, i) => 1500 + 300 * i
-        })
+    void main() {
+        vec2 uv = vUv.xy, uv2 = uv;
+        uv2.xy -= vec2(.5);
+        float s = sin(uTime), c = cos(uTime);
+        uv2 *= mat2(c, -s, s, c);
+        uv2.xy += vec2(.5);
+       
+        vec4 b = vec4(0.01, 0.05, 0.14, 1.);
+        vec4 p = vec4(0.13, 0.08, 0.13, 1.);
+        vec4 lb = vec4(0.10, 0.21, 0.33, 1.);
+        vec4 blb = mix(b, lb, -uv2.x * .2 - (uv2.y * .5));
+        vec4 col = mix(blb, p, uv2.x - (uv2.y * 1.5));
+        
+        float l = uv.x;
+        l*=l;
+        l*=l;
+        l*=l;
+        float r = 1.0 - uv.x;
+        r*=r;
+        r*=r;
+        r*=r;
+        float g = (l + r) * 0.1;
+        col += vec4(1.0) * g;
+        
+        gl_FragColor = col;
+    }
+  `
 }
 
 const count = 64
-
 const clock = new THREE.Clock()
 const dummy = new THREE.Object3D()
 const particles = []
 
-let camera, scene, renderer, instancedMesh, inputEvent
+let camera, scene, renderer, instancedMesh, auroraMesh, map, inputEvent
 
 function init() {
+    map = new THREE.TextureLoader().load('images/star.png', animate)
+}
+
+function initScene() {
     const width = window.innerWidth
     const height = window.innerHeight
 
@@ -87,7 +110,6 @@ function init() {
     scene = new THREE.Scene()
     scene.add(camera)
 
-    const map = new THREE.TextureLoader().load('images/star.png')
     const material = new THREE.MeshStandardMaterial({
         emissive: new THREE.Color(1, 1, 1),
         map: map,
@@ -96,7 +118,19 @@ function init() {
         blending: THREE.NormalBlending,
         opacity: 1.0
     })
+
     const geometry = new THREE.PlaneGeometry(1, 1, 1, 1)
+
+    const auroraMaterial = new THREE.ShaderMaterial({
+        uniforms: {
+            uTime: {value: 0}
+        },
+        fragmentShader: fragmentShader(),
+        vertexShader: vertexShader(),
+    })
+
+    auroraMesh = new THREE.Mesh(geometry, auroraMaterial)
+    scene.add(auroraMesh)
 
     instancedMesh = new THREE.InstancedMesh(geometry, material, count)
     instancedMesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage)
@@ -109,7 +143,9 @@ function init() {
 
     const container = document.getElementById('particles')
     container.appendChild(renderer.domElement)
+}
 
+function initInput() {
     let timeoutId
     const handleInputEvent = (timeout) => {
         timeoutId && clearTimeout(timeoutId)
@@ -162,22 +198,35 @@ function onWindowResize() {
 
 function animate() {
     requestAnimationFrame(animate)
+    update()
     render()
 }
 
-function render() {
+function update() {
     const delta = Math.min(clock.getDelta(), 1 / 30.0)
+    const time = clock.getElapsedTime()
+    updateParticles(time, delta)
+    updateAurora(time)
+}
+
+function updateAurora(time) {
+    auroraMesh.scale.set(window.innerWidth, window.innerHeight, 1)
+    auroraMesh.material.uniforms.uTime.value = time
+}
+
+function updateParticles(time, delta) {
     const vw = window.innerWidth
     const vh = window.innerHeight
     const aspect = vh / vw
-    const t = clock.getElapsedTime()
 
-    const tx = inputEvent ? inputEvent.clientX / vw : 0.5 + Math.sin(t * 0.881)
-    const ty = inputEvent ? 1.0 - inputEvent.clientY / vh : 0.5 + Math.cos(t * 0.487) * 0.75
+    const tx = inputEvent ? inputEvent.clientX / vw : 0.5 + Math.sin(time * 0.881)
+    const ty = inputEvent ? 1.0 - inputEvent.clientY / vh : 0.5 + Math.cos(time * 0.487) * 0.75
+    const speed = 0.15 + (1 + Math.cos(time * 0.4)) * 0.1
+    const size = 64
 
     for (let i = 0; i < count; ++i) {
         const particle = particles[i]
-
+        const scale = particle.scale
         const of = inputEvent ? 0.3 : 1
         const ox = tx + particle.r * aspect * of
         const oy = ty + particle.r2 * of
@@ -191,31 +240,44 @@ function render() {
         const vy = Math.sin(particle.rotation)
         const r90x = vy
         const r90y = vx * -1
-
-        const panic = inputEvent ? Math.min((l * l) * 32.0, 1.0) : Math.min(l * l - 0.2, 1.0)
-        const turnrate = inputEvent ? 3.0 : 0.75
-        const turn = particle.turnrate * turnrate * panic * delta
+        const panic = inputEvent ? Math.min(l * l * 32.0, 1.0) * 3.0 : Math.min(l * l - 0.2, 1.0) * 0.75
+        const turn = particle.turnrate * panic * delta
         const dot = r90x * nx + r90y * ny
 
-        if (dot > 0) {
-            particle.rotation += turn
-        } else {
-            particle.rotation -= turn
-        }
-
-        const speed = 0.15 + (1 + Math.cos(t * 0.4)) * 0.1
-        const scale = particle.scale
-
+        particle.rotation += dot > 0 ? turn : -turn
         particle.x += vx * speed * delta * scale * aspect
         particle.y += vy * speed * delta * scale
 
-        const size = 64
         dummy.position.set(particle.x * vw - vw * 0.5, particle.y * vh - vh * 0.5, 0)
         dummy.scale.set(size * scale, size * scale, 1)
         dummy.updateMatrix()
         instancedMesh.setMatrixAt(i, dummy.matrix)
     }
     instancedMesh.instanceMatrix.needsUpdate = true
+}
 
+function render() {
     renderer.render(scene, camera)
 }
+
+window.onload = function () {
+    anime.timeline({loop: false})
+        .add({
+            targets: '.word',
+            scale: [2, 1],
+            opacity: [0, 1],
+            translateY: ["50px", 0],
+            easing: 'easeOutCubic',
+            duration: 600,
+            delay: (el, i) => 1500 + 300 * i
+        })
+}
+
+function run() {
+    init()
+    initScene()
+    initInput()
+    initParticles()
+}
+
+run()
